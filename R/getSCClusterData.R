aeb1a2a7d80018228beabee7154a3b48dde184af
Laura Naslund <Naslund.Laura@epa.gov>
2026-03-31 08:37:31 -0400

    Add functionality to pull in StreamCat data used in clustering algorithm for a state

---
+++ R/getSCClusterData.R
@@ -0,0 +1,25 @@
+#' Get StreamCat data for clustering reaches
+#'
+#' @param state
+#'
+#' @return a wide dataframe of StreamCat variables used in the clustering algorithm for the state (not including the 300 m boundary)
+#' @export
+getSCClusterData <- function(state){
+  Sys.setenv("AWS_EC2_METADATA_DISABLED" = "true")
+
+  clust_ret <- data(list = "cluster_vars", package = "CASToolHelperPckg", envir = environment())
+  clust_vars <- get(clust_ret, envir = environment())
+
+  clust_vars_vec <- clust_vars |> dplyr::filter(Source == "StreamCat") |> dplyr::pull(Variable)
+
+  stateAbb <- state.abb[which(state.name == state)]
+
+  state_fp <- paste0("s3://dmap-data-commons-ow/streamcat/CASTool_State_SC/", stateAbb, "_CASTool_StreamCatMetrics.parquet")
+
+  state_pq <- arrow::open_dataset(state_fp) |>
+    dplyr::collect() |>
+    dplyr::rename_all(~ stringr::str_remove(.x, "ws$")) |>
+    dplyr::select(dplyr::all_of(c("comid", clust_vars_vec)))
+
+  return(state_pq)
+}
